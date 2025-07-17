676869d22af58b04bed5992d1ffab30e6242b907
Graham Hall <graham@ghall.dev>
2025-07-17 17:25:28 -0400

    🧪 Setup testing suite
    Installed Jest, set up basic tests

--- tests/game-by-id.test.ts
+++
@@ -1,9 +0,0 @@
-import { gameById } from '../src/index';
-
-async function main() {
-  const game = await gameById('68448', { stats: true });
-
-  console.log(JSON.stringify(game, null, 2));
-}
-
-main();
