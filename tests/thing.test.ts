5fbba719d63ede953dc528bd60a64a7dfee8c0e1
Graham Hall <graham@ghall.dev>
2025-08-04 09:01:38 -0400

    🧪 Update tests

--- tests/thing.test.ts
+++ tests/thing.test.ts
@@ -2,17 +2,18 @@ import bgg from '../src/index';
 
 describe('gameById', () => {
   test('7 Wonders', async () => {
-    const result = await bgg.thing('68448');
+    const result = await bgg.thing(68448);
     expect(result?.name.find(({ type }) => type === 'primary')?.value).toEqual(
       '7 Wonders',
     );
     expect(result?.statistics).toBeUndefined();
   });
-  test('7 Wonders with stats', async () => {
-    const result = await bgg.thing('68448', { stats: true });
+  test('7 Wonders with optional data', async () => {
+    const result = await bgg.thing(68448, { stats: true, versions: true });
     expect(result?.name.find(({ type }) => type === 'primary')?.value).toEqual(
       '7 Wonders',
     );
     expect(result?.statistics).toBeDefined();
+    expect(result?.versions).toBeDefined();
   });
 });
